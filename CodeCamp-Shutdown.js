0a94084653d923ded1530e10a8b49515fd6af592
Parker Barrett <parkerbarrett04@gmail.com>
2017-07-19 10:15:36 -0400

    hi

--- CodeCamp-Shutdown.js
+++ CodeCamp-Shutdown.js
@@ -10,7 +10,7 @@ module.exports = {
         // CODE HERE!
         // *********************************************************************
 
-        Slack.postMessageToChannel(channelName, "I am going to sleep. you better be carefull when YOU go to sleep.");
+        Slack.postMessageToChannel(channelName, "I am going to sleep. You better be carefull when YOU go to sleep.");
 
         // *********************************************************************
         // STOP CODING!
