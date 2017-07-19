bf17cd5ddfe3153e4468e12db4531c05939fd63b
Cortney Nelson <coolkidsnetwork@gmail.com>
2017-07-19 13:01:33 -0400

    Commit

--- CodeCamp-LoggedIn.js
+++ CodeCamp-LoggedIn.js
@@ -16,6 +16,7 @@ module.exports = {
         Slack.postMessageToChannel(channel, "Please use complete sentences.");
         Slack.postMessageToChannel(channel, 'Example: Hello.');
         Slack.postMessageToChannel(channel, "Start chatting with Boneless Biscuit Bot :smile: ");
+        
 
         // *********************************************************************
         // STOP CODING!
