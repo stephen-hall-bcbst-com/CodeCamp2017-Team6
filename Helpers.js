a2d0e9136524515b4a1aa70ea2a34bcfe2f40876
Stephen Hall <stephen_hall@bcbst.com>
2017-07-20 09:38:54 -0400

    Updating helpers

--- Helpers.js
+++ Helpers.js
@@ -22,4 +22,37 @@ module.exports = {
         }
         return data;
     },
+
+    /**
+     * searches an array for the given string value
+     * and returns true if found (false if not)
+     * 
+     * @param {Array} array 
+     * @param {string} value 
+     * @return {boolean} true if found, false if not found
+     */
+    searchArray: function searchArray(array, value) {
+        for (var x = 0; x < array.length; x++) {
+            if (array[x] == value) {
+                return true;
+            }
+        }
+        
+        return false;
+    },
+
+    /**
+     * 
+     * @param {string} text
+     * @return {string} Returns the last word in a string
+     */
+    getLastWord: function getLastWord(text) {
+        var lastSpace = text.lastIndexOf(' ');
+
+        if (lastSpace == 0 || lastSpace >= text.length) {
+            return '';
+        }
+        
+        return text.substring(lastSpace);
+    },
 };
