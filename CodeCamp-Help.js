0f42d5e1c1e981495defdaf17492ec13449322ee
Ezra Howard <21.howard.ezra@csasupper.com>
2017-07-20 14:32:20 -0400

    more responses help

--- CodeCamp-Help.js
+++ CodeCamp-Help.js
@@ -21,8 +21,11 @@ module.exports = {
          var replies = [
             "Would you like to ask a question to the magic 8 ball :8ball: ? If so, go right ahead!", 
             "How can I help you? Ask me anything! :8ball: ", 
-            "Do you need a question answered? Ask me, don't be shy! :8ball: "];
-            "Heard your cry for help! :8ball: ";
+            "Do you need a question answered? Ask me, don't be shy! :8ball: ",
+            "Heard your cry for help! :8ball: ", 
+            'Ask me what I know about you! example: "What do you know about me?',
+            'Ask me what time it is! example: "What do you know about me? ',
+            ];
 
         var response = helper.phraseAtRandom(replies);
 
