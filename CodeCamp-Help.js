b2e5252c6c35999696e1bcf92754525d7813701c
Cortney Nelson <coolkidsnetwork@gmail.com>
2017-07-20 09:45:11 -0400

    Help Additions

--- CodeCamp-Help.js
+++ CodeCamp-Help.js
@@ -19,6 +19,7 @@ module.exports = {
             "Would you like to ask a question to the magic 8 ball :8ball: ? If so, go right ahead!", 
             "How can I help you? Ask me anything! :8ball: ", 
             "Do you need a question answered? Ask me, don't be shy! :8ball: "];
+            "Heard your cry for help! :8ball: ";
 
         var response = helper.phraseAtRandom(replies);
 
